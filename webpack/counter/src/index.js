93cd41a6dd7921bd31e20efbc828904efa60a351
Makvi-droid <pasbenauro05@gmail.com>
2025-07-12 21:21:48 +0800

    practicing asynchronous js

--- webpack/counter/src/index.js
+++ webpack/counter/src/index.js
@@ -1,3 +1,18 @@
 import { increment, decrement, reset } from "./counter.js";
 
-console.log(increment());
+const addBtn = document.getElementById("increment");
+const subtractBtn = document.getElementById("decrement");
+const resetBtn = document.getElementById("reset");
+const currentNumber = document.getElementById("currentNumber");
+
+addBtn.addEventListener("click", () => {
+  currentNumber.innerHTML = increment();
+});
+
+subtractBtn.addEventListener("click", () => {
+  currentNumber.innerHTML = decrement();
+});
+
+resetBtn.addEventListener("click", () => {
+  currentNumber.innerHTML = reset();
+});
